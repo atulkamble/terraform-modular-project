e22639a7de9a87962ddaa8365a91fae2baeaae39
Atul Kamble <atul_kamble@hotmail.com>
2026-08-18 09:37:24 +0530

    code-update

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "ec2_instance_id" {
 output "ec2_public_ip" {
   value = module.web_server.public_ip
 }
+
+output "dev_instance_id" {
+  value = module.dev.instance_id
+}
+
+output "dev_public_ip" {
+  value = module.dev.public_ip
+}
+
+output "prod_instance_id" {
+  value = module.prod.instance_id
+}
+
+output "prod_public_ip" {
+  value = module.prod.public_ip
+}
